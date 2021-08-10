ff9be3b7d3bf54a4a7034746ccd2f83eed75640f
Jay Barbhaiya <jaybarb1990@gmail.com>
2021-08-10 22:11:19 +0200

    Adding initial UI elements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import UserInput from "./UserInput.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="App-content">
+				<div className="App-Input-content">
+					<UserInput />
+				</div>
+
+				<div className="App-Result-content">
+					<h3>Result content</h3>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/UserInput.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./UserInput.css";
+
+import { Button } from "@material-ui/core";
+
+function UserInput() {
+	var getFile = (oEvent) => {
+		const input = oEvent.target;
+		if ("files" in input && input.files.length) {
+			placeFileContent(
+				document.querySelector("#App-Result-content"),
+				input.files[0]
+			);
+		}
+	};
+
+	var placeFileContent = (target, file) => {
+		readFileContent(file)
+			.then((content) => {
+				target.value = content;
+			})
+			.catch((error) => console.log(error));
+	};
+
+	var readFileContent = (file) => {
+		const oFileReader = new FileReader();
+		return new Promise((resolve, reject) => {
+			oFileReader.onload = (oEvent) => resolve(oEvent.target.result);
+			oFileReader.onerror = (error) => reject(error);
+			oFileReader.readAsDataURL(file);
+		});
+	};
+
+	return (
+		<div className="UserInput">
+			<input
+				accept="image/png, image/jpeg"
+				className="userInput-input"
+				id="contained-button-file"
+				type="file"
+				name="imageInput"
+				onInput={getFile}
+			/>
+			<label htmlFor="contained-button-file">
+				<Button variant="contained" color="primary" component="span">
+					Upload
+				</Button>
+			</label>
+		</div>
+	);
+}
+
+export default UserInput;
